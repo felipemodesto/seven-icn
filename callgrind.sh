3b8717601390142b049faaebc6d202bd2f830ee5
felipe_modesto <felipe_modesto@localhost>
2017-02-09 12:53:52 -0500

    .

--- callgrind.sh
+++ callgrind.sh
@@ -4,4 +4,4 @@
 cd /home/felipe/Simulation/BaconNet/scenario/unified
 #
 #Callgrind - Call-graph tool & Cache + Branch Prediction Profiler
-valgrind --tool=callgrind --log-file="../../callgrind.log.out" /home/felipe/Simulation/omnetpp-5.0/bin/opp_run -r 20 -u Cmdenv -c Line_Edge -n ../../examples/veins:..:../../src/paradise:../../src/veins --image-path=../../images -l ../../src/veins --record-eventlog=false --debug-on-errors=false baconnet.ini
+valgrind --tool=callgrind --log-file="../../callgrind.interactive.out" /home/felipe/Simulation/omnetpp-5.0/bin/opp_run -r 20 -u Cmdenv -c Line_Edge -n ../../examples/veins:..:../../src/paradise:../../src/veins --image-path=../../images -l ../../src/veins --record-eventlog=false --debug-on-errors=false baconnet.ini
