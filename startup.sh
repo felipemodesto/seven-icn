8282d8f4eed766db47d4477e026cf83d1dc6f480
felipe_modesto <felipe_modesto@localhost>
2017-05-03 12:31:35 -0400

    Small changes for command-line sumo server startup and baconnet mod for non-dropbox installation.

---
+++ startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo " --> System Name is $HOSTNAME"
+if [ "Brick" = "${HOSTNAME}" ]; then
+	echo "Environment set to [    Brick     ]"
+	/home/felipe/Dropbox/uOttawa/Research/Code/VEINS_5/sumo-launchd.py -vv -c sumoD
+elif [ "Ubuntu" = "${hostname}" ]; then
+	echo "Environment set to [    Ubuntu    ]"
+	/home/felipe/Dropbox/uOttawa/Research/Code/VEINS_5/sumo-launchd.py -vv -c sumoD
+elif [ "OptiPlex" = "${HOSTNAME}" ]; then
+	echo "Environment set to [   OptiPlex   ]"
+	/home/felipe/Dropbox/uOttawa/Research/Code/VEINS_5/sumo-launchd.py -vv -c sumoD
+elif [ "Optiplex-7010" = "${HOSTNAME}" ]; then
+	echo "Environment set to [OptiPlex-7010 ]"
+	/home/felipe/Dropbox/uOttawa/Research/Code/VEINS_5/sumo-launchd.py -vv -c sumo
+elif [ "OptiPlex-7010-hamid" = "${HOSTNAME}" ]; then
+	echo "Environment set to [OptiPlex-hamid]"
+	/home/felipe/Dropbox/uOttawa/Research/Code/VEINS_5/sumo-launchd.py -vv -c sumoD
+elif [ "MacBook-Air.local" = "${HOSTNAME}" ]; then
+	echo "Environment set to [   Mac OS X   ]"
+	/Users/felipe/Dropbox/uOttawa/Research/Code/VEINS_5/sumo-launchd.py -vv -c sumoD
+else
+	echo "Envorinment set to [   Unknown    ] --> (${HOSTNAME})"
+	~/Simulation/BaconNet/sumo-launchd.py -vv -c sumo &
+fi
+
