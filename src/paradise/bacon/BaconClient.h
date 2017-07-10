e215b563d659ad027713c717fee8b2a2a5703992
felipe_modesto <felipe_modesto@localhost>
2017-07-10 10:28:52 -0400

    emergency switch to 300s simulations for 300 vehicles

---
+++ src/paradise/bacon/BaconClient.h
@@ -0,0 +1,159 @@
+//
+// Copyright (C) 2006-2011 Christoph Sommer <christoph.sommer@uibk.ac.at>
+//
+// Documentation for these modules is at http://veins.car2x.org/
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef BaconClient_H
+#define BaconClient_H
+
+#include <paradise/bacon/BaconContentProvider.h>
+#include <paradise/bacon/BaconLibrary.h>
+#include <paradise/bacon/BaconStructures.h>
+
+class BaconContentProvider;
+class BaconLibrary;
+class BaconStatistics;
+
+using namespace omnetpp;
+//using namespace osgEarth;
+//using namespace osgEarth::Annotation;
+//using namespace osgEarth::Features;
+
+/**
+ * Application Layer Service Manager
+ */
+//class BaconClient : public BaseWaveApplLayer , public IMobileNode {
+class BaconClient : public BaseWaveApplLayer {
+
+    public:
+        ~BaconClient();
+        virtual void initialize(int stage) override;
+        virtual void finish();
+        virtual void receiveSignal(cComponent* source, simsignal_t signalID, cObject* obj) override;
+
+        //double getX() const override { return x; }
+        //double getY() const override { return y; }
+        //double getLatitude() const override { return OsgEarthScene::getInstance()->toLatitude(y); }
+        //double getLongitude() const override { return OsgEarthScene::getInstance()->toLongitude(x); }
+        //double getTxRange() const override { return txRange; }
+
+        bool suggestContentRequest(Content_t* suggestedContent);
+        Coord getPosition();
+        int getID() { return myId; }
+
+    protected:
+        //==========================================//
+        //BASE STUFF
+        static const simsignalwrap_t parkingStateChangedSignal;
+        TraCIMobility* traci;
+        AnnotationManager* annotations;
+        simtime_t lastDroveAt;
+
+        //==========================================//
+//        //OSG EARTH STUFF
+//        // the node containing the osgEarth data
+//        osg::observer_ptr<osgEarth::MapNode> mapNode = nullptr;
+//        // osgEarth node for 3D visualization
+//        osg::ref_ptr<osgEarth::Util::ObjectLocatorNode> locatorNode = nullptr;
+//        // range indicator node
+//        osg::ref_ptr<osgEarth::Annotation::CircleNode> rangeNode = nullptr;
+//        // trail annotation
+//        osg::ref_ptr<osgEarth::Annotation::FeatureNode> trailNode = nullptr;
+//        osgEarth::Style trailStyle;
+//        osgEarth::Vec3dVector trail;  // recently visited points
+//        std::string modelURL;
+//        std::string labelColor;
+//        std::string rangeColor;
+//        std::string trailColor;
+//        unsigned int trailLength;
+//        bool showTxRange;
+//        double txRange;
+//        // node position and heading (speed is constant in this model)
+//        double heading;  // in degrees
+//        double x, y;  // in meters, relative to playground origin
+
+
+        //==========================================//
+        //VEINS MOBILITY STUFF
+
+        cMessage *runtimeTimer;             //Timer used for second to second updates
+        cMessage *contentTimerMessage;      //Self message sent in timer
+        simtime_t requestTimer;             //Time until user demands content
+
+        //==========================================//
+        //MY STUFF!
+
+        BaconStatistics* stats;
+        BaconLibrary* library;
+        BaconContentProvider* cache;
+
+        int clientExchangeIn;           //Input from Service Manager
+        int clientExchangeOut;          //Output to Service Manager
+
+        int maxOpenRequests;
+        double locationTimerDelay;      //Time between calls for location
+        double minimumRequestDelay;     //Minimum time until Content Request
+        double maximumRequestDelay;     //Maximum time until Content Request
+        double requestTimeout;          //
+
+        double multimediaInterest;      //Interest in Multimedia Content
+        double trafficInterest;         //Interest in Road Traffic Information Content
+        double networkInterest;         //Interest in Network Information Content
+        double emergencyInterest;       //Emergency Information "Interest"
+
+        //Last Known coordinates
+        double lastX;
+        double lastY;
+
+        NodeRole nodeRole = NodeRole::MULE;
+
+        //PendingContent_t* pendingRequest;//Current Pending Request
+        std::list<PendingContent_t*> ongoingRequests;       //List of ongoing Requests
+        std::list<PendingContent_t*> backloggedRequests;    //List of content to which a negative reply was given, hopefully so that we can track late responses somehow
+        std::list<PendingContent_t*> completedRequests;     //List of content to which a positive reply was given, so we can track duplicate responses
+
+    protected:
+
+        virtual int numInitStages () const {
+            return 3;
+        }
+
+        //virtual void refreshDisplay() const override;
+        //==========================================//
+        virtual void resetLocationTimer();                          //RESETS THE TIMER DELAY FOR LOCATION TIMER MESSAGE
+        virtual void notifyLocation();                              //NOTIFIES STATISTICS CLASS OF POSITION (SHOULD BE CALLED EVERY SECOND)
+        void cleanRequestList();                                    //Deletes old entries from our request lists
+        Content_t* selectObjectForRequest();                              //Selects the target for a request
+        //==========================================//
+        void onBeacon(WaveShortMessage* wsm)  override;             //ON WSM BEACON MESSAGE
+        void onData(WaveShortMessage* wsm)  override;               //ON WSM DATA MESSAGE
+        void handlePositionUpdate(cObject* obj) override;
+        void sendWSM(WaveShortMessage* wsm)  override;
+        void sendMessage(std::string messageContent);               //USED TO SEND WSM MESSAGES
+        void handleParkingUpdate(cObject* obj);
+        //==========================================//
+        void startNewMessageTimer(simtime_t timerTime);
+        void startNewMessageTimer();
+        bool startContentRequest();
+        bool startContentRequest(Content_t* preferedRequest);
+        void handleMessage(cMessage *msg)  override;                //CALLBACK FROM SELF MESSAGE, GENERALLY USED AS RANDOM TIMER CALLBACK
+        void sendToServiceManager(cMessage *msg);
+};
+
+
+#endif
---
+++ src/paradise/bacon/BaconLibrary.h
@@ -0,0 +1,128 @@
+/*
+ * BaconLibrary.hh
+ *
+ *  Created on: Jan 25, 2016
+ *      Author: felipe
+ */
+
+#ifndef BACONLIBRARY_H
+#define BACONLIBRARY_H
+
+#include <paradise/bacon/BaconClient.h>
+#include <paradise/bacon/BaconStatistics.h>
+#include <paradise/bacon/BaconStructures.h>
+
+class BaconStatistics;
+class BaconClient;
+
+using namespace omnetpp;
+using namespace std;
+
+//Content Library. Generates random content list and provides to whomever needs for simulation purposes. (Singleton)
+class BaconLibrary : public omnetpp::cSimpleModule {
+
+private:
+    //Keeping copies from appearing
+    //BaconLibrary(){};
+    //BaconLibrary(BaconLibrary const& copy){};     //This should not be implemented
+    //void operator = (BaconLibrary const& copy){}; //This should not be implemented (either)
+
+protected:
+
+    virtual void initialize(int stage);
+    virtual void finish();
+
+    void handleMessage(cMessage *msg);
+
+    virtual int numInitStages () const {
+        return 2;
+    }
+
+    //Library Mathematical Information
+    ContentCategoryDistribution_t multimediaContent;
+    ContentCategoryDistribution_t trafficContent;
+    ContentCategoryDistribution_t networkContent;
+    ContentCategoryDistribution_t emergencyContent;
+    long int currentIndex;
+
+    std::list<Content_t>* multimediaLibrary;
+    std::list<Content_t>* networkLibrary;
+    std::list<Content_t>* trafficLibrary;
+
+    std::vector<double> multimediaCummulativeProbabilityCurve;
+    std::vector<double> networkCummulativeProbabilityCurve;
+    std::vector<double> trafficCummulativeProbabilityCurve;
+
+    std::vector<double> buildCategoryLibrary(int count, int byteSize, ContentPriority priority, ContentClass category, std::string classPrefix);
+    void buildContentList();
+
+    LocationCorrelationModel locationModel;
+    int sectorCount = 1;
+    int widthBlocks = 1;
+    int heightBlocks = 1;
+    double sectorWidth = 250;       //Unit = Meters
+    double sectorHeight = 200;      //Unit = Meters
+
+    int maxVehicleServers = 0;
+    int allocatedVehicleServers = 0;
+    int maxVehicleClients = -1;
+    int allocatedVehicleClients = 0;
+
+    double zipfCaracterization = 0;
+    int sizeMultimedia = 1024;          //Packet size
+    int sizeNetwork = 1024;             //Packet size
+    int sizeTransit = 1024;             //Packet size
+
+    int libraryTransit = 10000;         //Library size
+    int libraryNetwork = 10000;         //Library size
+    int libraryMultimedia = 10000;      //Library size
+
+    ContentPriority priorityTransit;            //Priority
+    ContentPriority priorityNetwork;            //Priority
+    ContentPriority priorityMultimedia;         //Priority
+
+    std::list<int> serverVehicles;
+    std::list<int> clientVehicles;
+
+    cMessage* requestTimer;             //Running Timer
+    std::list<LocationRequest_t> preemptiveRequests;
+    string requestSequenceFile;
+
+    BaseWorldUtility *world;
+    BaconStatistics* stats;
+
+public:
+    std::string transitPrefix = "t";
+    std::string networkPrefix = "n";
+    std::string multimediaPrefix = "m";
+
+    bool independentOperationMode();
+    void setupPendingRequests();
+    void loadRequestSequence();
+    void registerClient(string clientPath);
+    void deregisterClient(string clientPath);
+    std::list<string> clientList;
+
+    NodeRole requestStatus(int vehicleID);
+    void releaseStatus(NodeRole status, int vehicleID);
+    int getActiveServers();
+    int getMaximumServers();
+
+    long int getCurrentIndex();                      //For Statistics & Maximum Count Use
+    long int getRequestIndex();                      //Returns next index (sequence ID) in global request list numbering scheme (to avoid doubles)
+    Content_t* getContent(std::string prefix);
+    std::list<Content_t>* getMultimediaContentList();
+    std::list<Content_t>* getTrafficContentList();
+    std::list<Content_t>* getNetworkContentList();
+
+    //float BaconLibrary::getDensityForPrefix(string itemPrefix);
+    float getDensityForIndex(double index,ContentClass contentClass);
+    int getIndexForDensity(double value, ContentClass contentClass);
+    int getIndexForDensity(double value, ContentClass contentClass, int sector );
+    int getIndexForDensity(double value, ContentClass contentClass, double xPos, double yPos );
+    int getContentClass(ContentClass cClass);
+
+    static std::string cleanString(std::string inputString);
+};
+
+#endif /* BACONLIBRARY_H */
