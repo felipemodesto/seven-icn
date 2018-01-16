255baed0c4e05197c8b486e0b69e707f0b9bae55
felipe_modesto <felipe_modesto@localhost>
2018-01-16 16:37:02 -0500

    Fixed annoying warning messages for library search, set simulation stuff

--- massif.sh
+++ massif.sh
@@ -5,4 +5,4 @@ make MODE=debug
 cd /home/felipe/Simulation/BaconNet/scenario/unified
 #
 #Memory Allocation mapping
-valgrind --tool=massif --massif-out-file="../../massif.log.out" /home/felipe/Simulation/omnetpp-5.0/bin/opp_run -r 0 -c Ottawa_50 -u Cmdenv -n ..:../../src/paradise:../../src/veins --image-path=../../images -l ../../src/veins --record-eventlog=false --debug-on-errors=false -f baconnet.ini
+valgrind --tool=massif --massif-out-file="../../massif.log.out" /home/felipe/Simulation/omnetpp-5.0/bin/opp_run -r 1 -c Ottawa_50 -u Cmdenv -n ..:../../src/paradise:../../src/veins --image-path=../../images -l ../../src/veins --record-eventlog=false --debug-on-errors=false -f baconnet.ini
